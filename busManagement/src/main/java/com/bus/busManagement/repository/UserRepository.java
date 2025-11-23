e48bebc0555bd9e6ded7cc7d8feb313bccdae67c
dinukaPeramuna <dinukac7pro@gmail.com>
2025-11-23 09:00:31 +0000

    fourth commit

--- busManagement/src/main/java/com/bus/busManagement/repository/UserRepository.java
+++ busManagement/src/main/java/com/bus/busManagement/repository/UserRepository.java
@@ -1,5 +1,12 @@
-package com.bus.busManagement.repository;
+package com.bus.system.repository;
 
-public class UserRepository {
-    
+import com.bus.system.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
 }
