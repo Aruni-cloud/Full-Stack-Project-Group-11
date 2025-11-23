887857424e82d430e3ec7219417de409f6106c5b
dinukaPeramuna <dinukac7pro@gmail.com>
2025-11-23 09:02:21 +0000

    fiifth commit

--- busManagement/src/main/java/com/bus/busManagement/repository/TripRepository.java
+++ busManagement/src/main/java/com/bus/busManagement/repository/TripRepository.java
@@ -1,5 +1,12 @@
-package com.bus.busManagement.repository;
 
-public class TripRepository {
-    
+package com.bus.system.repository;
+
+import com.bus.system.model.Trip;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface TripRepository extends MongoRepository<Trip, String> {
+    List<Trip> findByStatus(String status); 
 }
