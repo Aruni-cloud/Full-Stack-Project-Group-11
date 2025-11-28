51ebb9e05e0b627ceec404dc54b4b47fbbfe3967
Chamodya-Samarakoon <chamodyasamarakoon202@gmail.com>
2025-11-28 10:13:34 +0530

    add AdminRepository.java, Bus.java,driver.java,Complaint.java

--- HighwayBusManagementSystem/src/main/java/com/example/HighwayBusManagementSystem/Model/Complaint.java
+++ HighwayBusManagementSystem/src/main/java/com/example/HighwayBusManagementSystem/Model/Complaint.java
@@ -1,5 +1,21 @@
 package com.example.HighwayBusManagementSystem.Model;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "complaints")
 public class Complaint {
 
+    @Id
+    private String id;
+
+    private String userName;
+    private String email;
+    private String message;
+    private String status; // Pending / Resolved
+    private String date;
 }
