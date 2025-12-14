921b58153f922cec4dcffe662094fa8da5aa5fe9
Chamodya-Samarakoon <chamodyasamarakoon202@gmail.com>
2025-12-14 18:16:58 +0530

    add security and services

---
+++ src/main/java/com/example/bus_backend/services/AdminService.java
@@ -0,0 +1,5 @@
+package com.example.bus_backend.services;
+
+public class AdminService {
+
+}
---
+++ src/main/java/com/example/bus_backend/services/impl/AdminAuthServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.bus_backend.services.impl;
+
+public interface AdminAuthServiceImpl {
+
+}
